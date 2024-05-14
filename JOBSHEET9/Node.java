a9c1bdc918d0607fccfe3de9b694d70cddfc48f9
faishal-ai <okbos6337@gmail.com>
2024-05-14 19:36:53 +0700

    jobsheet9

---
+++ JOBSHEET9/Node.java
@@ -0,0 +1,12 @@
+package JOBSHEET9;
+
+public class Node {
+        int data;
+        Node next;
+        Node(int nilai, Node berikutnya) { 
+            data = nilai;
+            next = berikutnya;
+        }
+        
+}
+
---
+++ JOBSHEET9/SingleLinkedList.java
@@ -0,0 +1,183 @@
+package JOBSHEET9;
+
+public class SingleLinkedList {
+    Node head, tail;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            System.out.print("Isi Linked List : ");
+            while (tmp != null) {
+
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("\n");
+        } else {
+            System.out.println("Linked List kosong");
+        }
+    }
+
+    void addFirst(int input) {
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    void addLast(int input) {
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    void insertAfter(int key, int input) {
+        Node ndInput = new Node(input, null);
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput;
+                }
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    void insertAt(int index, int input) {
+        Node ndInput = new Node(input, null);
+        if (index < 0) {
+            System.out.println("Indeks tidak valid!");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1 && temp != null; i++) {
+                temp = temp.next;
+            }
+            if (temp != null) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput;
+                }
+            } else {
+                System.out.println("Indeks melebihi panjang Linked List!");
+            }
+        }
+    }
+
+    int getData(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Indeks tidak valid!");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            if (tmp == null) {
+                throw new IndexOutOfBoundsException("Indeks melebihi panjang linked list!");
+            }
+            tmp = tmp.next;
+        }
+        if (tmp == null) {
+            throw new IndexOutOfBoundsException("Indeks melebihi panjang linked list!");
+        }
+        return tmp.data;
+    }
+
+    int indexOf(int key) {
+        Node tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp != null) {
+            return index;
+        } else {
+            return -1;
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head.data == key) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    return;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    void removeAt(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Indeks tidak valid!");
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp == null || temp.next == null) {
+                throw new IndexOutOfBoundsException("Indeks melebihi  panjang linked list!");
+            }
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        if (temp.next == null) {
+            tail = temp;
+        }
+    }
+
+}
